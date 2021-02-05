a53ffe712af4d69801e8efef5acc6a7056789080
Nurkyz <rahmanalievan181@gmail.com>
2021-02-05 18:56:47 +0600

    05-02 sabak from react

---
+++ src/to-do/todo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Body from "./body"
+import Title from "./title"
+const Todo = () => {
+    return <div>
+        <Title/>
+        <Body/>
+    </div>
+  }
+
+export default Todo
